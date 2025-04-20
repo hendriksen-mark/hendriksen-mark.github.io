5fb05ceb16cc01eb5deeb80988316f31135d0606
Mark <hendriksen-mark@hotmail.com>
2025-04-20 14:06:57 +0200

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src', // Set the root directory to 'src'
   build: {
+    outDir: '../dist', // Output build files to the 'dist' folder
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].js',
