411c3e78f02bb0ca9d2501f1f2fdf0a0f058e08a
Mark <hendriksen-mark@hotmail.com>
2025-04-20 15:05:54 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+if (!global.crypto) {
+  global.crypto = require('crypto'); // Use Node.js built-in crypto module
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
